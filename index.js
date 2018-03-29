6c65b59e10fc269b1da86e7d1e1fa4fa949d8a59
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2018-03-28 21:29:29 -0300

    serverless

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict';
 
-
 const Buffer = require('safe-buffer').Buffer;
 
 // [START functions_helloworld_debug]
 require('@google-cloud/debug-agent').start();
 // [END functions_helloworld_debug]
 
-// [START functions_helloworld]
 /**
  * Cloud Function.
  *
@@ -18,21 +16,8 @@ exports.helloWorld = function helloWorld (event, callback) {
   console.log(`My Cloud Function: ${event.data.message}`);
   callback();
 };
-// [END functions_helloworld]
 
-// [START functions_helloworld_get]
-/**
- * HTTP Cloud Function.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
-exports.helloGET = function helloGET (req, res) {
-  res.send('oi sumido');
-};
-// [END functions_helloworld_get]
 
-// [START functions_helloworld_http]
 /**
  * HTTP Cloud Function.
  *
